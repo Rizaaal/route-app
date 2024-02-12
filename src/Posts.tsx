70e5fa9ef04f69a33ffac1e1a87fff4672e479cf
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-12 22:36:31 +0100

    feat: added Posts component

---
+++ src/Posts.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Posts(){
+  const [data, setData] = useState<Array<Post>>([]);
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => setData(json))
+  }, []);
+
+  const postStyle = {
+    backgroundColor: "rgb(69, 77, 91)",
+    padding: "1em",
+    margin: ".5em",
+    borderRadius: "4px"
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post: Post) => 
+          <li key={post.id} style={postStyle}>
+            <NavLink to={`/${post.id}`} className="App-Link">{post.title}</NavLink> 
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default Posts
