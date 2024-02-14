51d300adecd8d447eba399a6bed88be3bf99a4a4
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-14 13:13:56 +0100

    feat: improved links and other styled

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export function Post(){
+export function Post() {
   const { id } = useParams();
   const [post, getPostData] = useState<Post | null>(null);
 
@@ -9,12 +9,12 @@ export function Post(){
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-    .then(response => response.json())
-    .then(json => getPostData(json));
+      .then(response => response.json())
+      .then(json => getPostData(json));
 
-    fetch('https://jsonplaceholder.typicode.com/posts/' +id +'/comments')
-    .then(response => response.json())
-    .then(json => getComments(json));
+    fetch('https://jsonplaceholder.typicode.com/posts/' + id + '/comments')
+      .then(response => response.json())
+      .then(json => getComments(json));
   }, []);
 
   const commentStyle = {
@@ -24,21 +24,25 @@ export function Post(){
   }
 
   return (
-      post ? 
-      <div className="Post">
-        <h1>{post.title}</h1>
-        <p>{post.body}</p>
-        <h2>Comments</h2>
-        <ul>
-          {comments.map(comment => 
-            <li key={comment.id} style={commentStyle}>
-              <p>{comment.name}</p>
-              <p>{comment.email}</p>
-              <p>{comment.body}</p>
-            </li>
-          )}
-        </ul>
-      </div>
-      : <p>loading...</p>
+    <>
+      {post ?
+        <>
+          <div className="Post" style={{margin: '1em'}}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+          <ul className="Comments" style={{margin: '1em'}}>
+            <h2>Comments</h2>
+            {comments.map(comment =>
+              <li key={comment.id} style={commentStyle}>
+                <p>{comment.name}</p>
+                <p>{comment.email}</p>
+                <p>{comment.body}</p>
+              </li>
+            )}
+          </ul>
+        </>
+        : <p>loading...</p>}
+    </>
   );
 }
