5208935154981e4bc9201a4a9fd5c5cd7ab03659
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-15 21:29:16 +0100

    refactor: replaced inline styled with styled-components

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 function Posts(){
   const cache: string | null = sessionStorage.getItem("data");
@@ -20,21 +21,24 @@ function Posts(){
 
   }, []);
 
-  const postStyle = {
-    backgroundColor: "rgb(69, 77, 91)",
-    padding: "1em",
-    margin: ".5em",
-    borderRadius: "4px"
-  }
+  const Post = styled.li`
+    background-color: rgb(69, 77, 91);
+    padding: 1em;
+    margin: .5em;
+    border-radius: 4px;
+  `
+  
 
   return (
     <>
       <h1>Posts</h1>
       <ul>
         {data ? data.map((post: Post) => 
-          <li key={post.id} style={postStyle}>
-            <NavLink to={`/post/${post.id}`} className="App-link">{post.title}</NavLink> 
-          </li>
+          <Post key={post.id}>
+            <NavLink to={`/post/${post.id}`} className="App-link">
+              {post.title}
+            </NavLink> 
+          </Post>
         ) : <p>Loading...</p>}
       </ul>
     </>
